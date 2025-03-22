33a162c62d1c6d49c5ecf6b6c0a5cdd5d83da75c
u1_mas <mtakuya1022@gmail.com>
2025-03-22 16:14:10 +0900

    [🐛 fix] dotenvのパッケージ名を修正 (from cursor) - @std/dotenvに変更

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { load } from "dotenv";
+import { load } from "@std/dotenv";
 import { bot } from "./src/bot.ts";
 
 // 環境変数の読み込み
@@ -11,13 +11,4 @@ try {
 } catch (error) {
   console.error("Error starting bot:", error);
   Deno.exit(1);
-}
-
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
 }
