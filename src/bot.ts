46aefc5a55c7277e0d18800796eaa039e7d36ce6
u1_mas <mtakuya1022@gmail.com>
2025-03-22 17:24:47 +0900

    update

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Bot, createBot, GatewayIntents } from "discordeno";
+import { type Bot, createBot, Intents } from "discordeno";
 
 // 環境変数からトークンを取得
 const token = Deno.env.get("DISCORD_TOKEN");
@@ -9,10 +9,8 @@ if (!token) {
 // ボットの作成
 export const bot: Bot = createBot({
   token,
-  intents: GatewayIntents.Guilds |
-    GatewayIntents.GuildVoiceStates |
-    GatewayIntents.GuildMessages |
-    GatewayIntents.MessageContent,
+  intents: Intents.Guilds | Intents.GuildVoiceStates | Intents.GuildMessages |
+    Intents.MessageContent,
   events: {
     ready: (_bot, payload) => {
       console.log(`Logged in as ${payload.user.username}!`);
