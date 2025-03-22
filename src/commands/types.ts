9a342ab0ba794252328b0ca9aff8ea228b52b6a5
u1_mas <mtakuya1022@gmail.com>
2025-03-22 19:40:53 +0900

    [feat] スラッシュコマンドの型定義を追加 (from cursor)

---
+++ src/commands/types.ts
@@ -0,0 +1,8 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+
+export interface Command {
+    data:
+        | SlashCommandBuilder
+        | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
+    execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
+}
